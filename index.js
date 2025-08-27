01b0bd6d5af2533ed72cf5a96c1e632febfd7569
Thecla Owano <teckowano@gmail.com>
2025-08-27 22:16:41 +0300

    Java Test

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 30;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+console.log(message); 
+
+describe("Name", () => {
+  it('returns "Susan"', () => {
+    expect(name).toEqual("Susan");
+  });
+});
+
+describe("Height", () => {
+  it("is less than 40", () => {
+    expect(height).toBeLessThan(40);
+  });
+});
+
+describe("Message", () => {
+  it("gives the name and height", () => {
+    expect(message).toInclude(name);
+    expect(message).toInclude(height);
+  });
+});
+
+describe('', () => {
+  describe('', () => {
+    it('', () => {
+      return true;
+    })
+  })
+})
+
+module.exports = { name, height, message };
+console.log(message);
